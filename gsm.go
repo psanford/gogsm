858abb4fd0476282ff483f6f51ea6fcec4dd5487
Peter Sanford <psanford@sanford.io>
2021-12-05 16:54:36 -0800

    Handle async events

--- gsm.go
+++ gsm.go
@@ -3,11 +3,14 @@ package gogsm
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"encoding/csv"
 	"errors"
 	"fmt"
+	"log"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/tarm/serial"
@@ -24,22 +27,73 @@ func NewModem(serialPort string) (*Modem, error) {
 	}
 
 	m := Modem{
-		conn:  s,
-		lines: make(chan string, 10),
+		conn:       s,
+		lines:      make(chan string, 10),
+		asyncLines: make(chan string, 10),
 	}
 	go m.lineReader()
+	go m.handleAsyncLines()
 
 	return &m, nil
 }
 
 type Modem struct {
-	conn    *serial.Port
-	lines   chan string
-	readErr error
+	conn          *serial.Port
+	lines         chan string
+	asyncLines    chan string
+	readErr       error
+	mu            sync.Mutex
+	eventHandlers []eventHandler
 }
 
 var CommandError = errors.New("cmd error")
 var CommandNotSupportError = errors.New("cmd not support")
+
+func (m *Modem) handleAsyncLines() {
+	for line := range m.asyncLines {
+		if strings.HasPrefix(line, "^") {
+			continue
+		} else if strings.HasPrefix(line, "+CMTI:") {
+			m.mu.Lock()
+			var smsHandlers []eventHandler
+			for i, h := range m.eventHandlers {
+				if h.ctx.Err() != nil {
+					m.eventHandlers = append(m.eventHandlers[:i], m.eventHandlers[i+1:]...)
+				}
+				if h.evtType&EvtSMS > 0 {
+					smsHandlers = append(smsHandlers, h)
+				}
+			}
+			m.mu.Unlock()
+
+			if len(smsHandlers) > 0 {
+				// +CMTI: "ME",7
+				_, idx, err := parseCMTI(line)
+				if err != nil {
+					log.Printf("parseCMTI err: %s", err)
+					continue
+				}
+
+				msg, err := m.ReadMsg(idx)
+				if err != nil {
+					log.Printf("readMsg err: %s", err)
+					continue
+				}
+				evt := AsyncEvent{
+					Type: EvtSMS,
+					Msg:  *msg,
+				}
+
+				for _, h := range smsHandlers {
+					select {
+					case h.c <- evt:
+					case <-h.ctx.Done():
+					}
+				}
+			}
+		}
+	}
+}
 
 func (m *Modem) lineReader() {
 	r := bufio.NewReader(m.conn)
@@ -50,8 +104,16 @@ func (m *Modem) lineReader() {
 			close(m.lines)
 			return
 		}
-		// fmt.Printf("read: %s", line)
-		m.lines <- line
+		// log.Printf("read: %s", line)
+		if strings.HasPrefix(line, "^") || strings.HasPrefix(line, "+CMTI:") {
+			m.asyncLines <- line
+			continue
+		}
+		select {
+		case m.lines <- line:
+		default:
+			// log.Printf("line dropped")
+		}
 	}
 }
 
@@ -104,26 +166,70 @@ type Msg struct {
 	Body    string
 }
 
+type AsyncEventType int
+
+const (
+	EvtSMS AsyncEventType = 1 << iota
+	// EvtUSSD AsyncEventType = 1 << iota
+	// EvtDTMF AsyncEventType = 1 << iota
+)
+
+type AsyncEvent struct {
+	Type AsyncEventType
+	Msg  Msg
+}
+
+type eventHandler struct {
+	evtType AsyncEventType
+	c       chan AsyncEvent
+	ctx     context.Context
+}
+
+func (m *Modem) Subscribe(ctx context.Context, evtMask AsyncEventType) (chan AsyncEvent, error) {
+	if evtMask&EvtSMS > 0 {
+		_, err := m.Command("AT+CNMI=2,1,0,2")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	h := eventHandler{
+		evtType: evtMask,
+		c:       make(chan AsyncEvent),
+		ctx:     ctx,
+	}
+
+	m.mu.Lock()
+	m.eventHandlers = append(m.eventHandlers, h)
+	m.mu.Unlock()
+
+	return h.c, nil
+}
+
 func (m *Modem) DeleteMsg(idx int) error {
 	_, err := m.Command(fmt.Sprintf("AT+CMGD=%d", idx))
 	return err
 }
 
 func (m *Modem) ReadMessages() ([]Msg, error) {
-	var msgs []Msg
 	lines, err := m.Command("AT+CMGL=4")
 	if err != nil {
 		return nil, err
 	}
+	return m.parseMsgs(lines, "+CMGL: ")
+}
+
+func (m *Modem) parseMsgs(lines []string, prefix string) ([]Msg, error) {
+	var msgs []Msg
 	for i := 0; i < len(lines); i++ {
 		line := lines[i]
-		if !strings.HasPrefix(line, "+CMGL: ") {
+		if !strings.HasPrefix(line, prefix) {
 			continue
 		}
-		line = strings.TrimPrefix(line, "+CMGL: ")
+		line = strings.TrimPrefix(line, prefix)
 		record, err := csv.NewReader(bytes.NewBufferString(line)).Read()
 		if err != nil {
-			return nil, fmt.Errorf("parse +CMGL line %q err: %w", line, err)
+			return nil, fmt.Errorf("parse %s line %q err: %w", prefix, line, err)
 		}
 		idxS := record[0]
 		statusS := record[1]
@@ -132,12 +238,15 @@ func (m *Modem) ReadMessages() ([]Msg, error) {
 
 		idx, err := strconv.Atoi(idxS)
 		if err != nil {
-			return nil, fmt.Errorf("parse +CMGL idx %q err: %w", line, err)
+			return nil, fmt.Errorf("parse %s idx %q err: %w", prefix, line, err)
 		}
 
-		status, err := strconv.Atoi(statusS)
-		if err != nil {
-			return nil, fmt.Errorf("parse +CMGL status %q err: %w", line, err)
+		var status int
+		if prefix == "+CMGL: " {
+			status, err = strconv.Atoi(statusS)
+			if err != nil {
+				return nil, fmt.Errorf("parse %s status %q err: %w", prefix, line, err)
+			}
 		}
 
 		pduline := lines[i+1]
@@ -146,7 +255,7 @@ func (m *Modem) ReadMessages() ([]Msg, error) {
 
 		pdu, err := pdumode.UnmarshalHexString(pduline)
 		if err != nil {
-			return nil, fmt.Errorf("parse +CMGL pdu %q err: %w", pduline, err)
+			return nil, fmt.Errorf("parse %s pdu %q err: %w", prefix, pduline, err)
 		}
 
 		var ops []sms.UnmarshalOption
@@ -155,14 +264,14 @@ func (m *Modem) ReadMessages() ([]Msg, error) {
 		}
 		p, err := sms.Unmarshal(pdu.TPDU, ops...)
 		if err != nil {
-			return nil, fmt.Errorf("parse +CMGL pdu %q err: %w", pduline, err)
+			return nil, fmt.Errorf("parse %s pdu %q err: %w", prefix, pduline, err)
 		}
 
 		msg := Msg{
 			Index:   idx,
 			Status:  MsgStatus(status),
 			Inbound: p.Direction == tpdu.MT,
-			From:    pdu.SMSC.Number(),
+			From:    p.OA.Number(),
 			TS:      p.SCTS.Time,
 			Body:    string(p.UD),
 		}
@@ -170,6 +279,22 @@ func (m *Modem) ReadMessages() ([]Msg, error) {
 		msgs = append(msgs, msg)
 	}
 	return msgs, nil
+}
+
+func (m *Modem) ReadMsg(idx int) (*Msg, error) {
+	lines, err := m.Command(fmt.Sprintf("AT+CMGR=%d", idx))
+	if err != nil {
+		return nil, err
+	}
+	msgs, err := m.parseMsgs(lines, "+CMGR: ")
+	if err != nil {
+		return nil, err
+	}
+	if len(msgs) < 1 {
+		return nil, errors.New("no message found")
+	}
+	msg := msgs[0]
+	return &msg, nil
 }
 
 func (m *Modem) Connect() error {
@@ -213,4 +338,20 @@ func (m *Modem) write(b []byte) (int, error) {
 		return l - 2, err
 	}
 	return l, err
+}
+
+// Parse +CMTI lines (Incoming SMS event)
+func parseCMTI(line string) (string, int, error) {
+	line = strings.TrimPrefix(line, "+CMTI: ")
+	record, err := csv.NewReader(bytes.NewBufferString(line)).Read()
+	if err != nil {
+		return "", -1, fmt.Errorf("parse +CMTI line %q err: %w", line, err)
+	}
+
+	idx, err := strconv.Atoi(record[1])
+	if err != nil {
+		return "", 01, fmt.Errorf("parse +CMTI idx %q err: %w", line, err)
+	}
+
+	return record[0], idx, nil
 }
